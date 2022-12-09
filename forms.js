0a71298e1016121eecb5df7ef0f0d2c4332e7547
vaquino <virginia.aquino@gdpteam.com>
2022-12-08 19:56:23 -0500

    repaso intro vue

---
+++ forms.js
@@ -0,0 +1,41 @@
+const TextInput = {
+    props:{
+        name: String,
+        type:String,
+        label: String,
+        placeholder:String,
+        required: String,
+        min:String,
+        max: String,
+        value:String,
+    },
+    template:`
+        <div class="mb3">
+            <label :for="name" class="form-label">{{label}}</label>
+            <input
+                :type="type"
+                :name="name"
+                :placeholder="placeholder"
+                :required="required"
+                :min="min"
+                :max="max"
+                :value="value"
+                :autocomplete="name +'-new'"
+                class="form-control">
+        <div>
+    `
+}
+
+const SelectInput ={
+    props: ["items", "name","required","label"],
+    template:`
+        <div class="mb-3">
+           <label :for="name" class="form-label">{{label}}</label>
+           <select :id="name" class="form-select" :name="name" :required="required">
+               <option v-for="option in items" :value="option.value">
+                   {{option.text}}
+               </option>
+           </select>
+        </div>
+    `
+}
