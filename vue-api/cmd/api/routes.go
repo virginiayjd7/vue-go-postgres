008e9bd59e80d1bf4688d27075b41a24478e78bb
vaquino <virginia.aquino@gdpteam.com>
2022-12-12 08:27:50 -0500

    start with go

---
+++ vue-api/cmd/api/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// routes generates our routes and attaches them to handlers, using the chi router
+// note that we return type http.Handler, and not *chi.Mux; since chi.Mux satisfies
+// the interface requirements for http.Handler, it makes sense to return the type
+// that is part of the standard library.
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/users/login", app.Login)
+	mux.Post("/users/login", app.Login)
+
+	return mux
+
+}
---
+++ vue-api/go.mod
@@ -0,0 +1,5 @@
+module vue-api
+
+go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
